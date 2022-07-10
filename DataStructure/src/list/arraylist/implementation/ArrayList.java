f40a1ea736aaf5746cf073e2ed6ff756b55a0309
kim-hyoungwoo <dicjqdlwld@gmail.com>
2022-07-10 22:30:53 +0900

    소스트리 이클립스 깃 연동 테스트

---
+++ DataStructure/src/list/arraylist/implementation/ArrayList.java
@@ -0,0 +1,8 @@
+package list.arraylist.implementation;
+
+public class ArrayList {
+	// 몇개의 데이터가 리스트에 들어오는가.
+	private int size = 0;
+	// 배열로 ArrayList 만들기
+	private Object[] elementData = new Object[100];
+}
---
+++ DataStructure/src/list/arraylist/implementation/Main.java
@@ -0,0 +1,11 @@
+package list.arraylist.implementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList numbers = new ArrayList();
+
+	}
+
+}
