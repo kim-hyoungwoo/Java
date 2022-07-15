6c74c41e468a765816f330e244d54ba2b60b0706
kim-hyoungwoo <dicjqdlwld@gmail.com>
2022-07-15 22:15:47 +0900

    프로그래머스 알고리즘 공부

---
+++ Programmers/src/algorithm/Main.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * 전화번호 목록
+		 */
+		String[] phone_book = {"119", "97674223", "1195524421"};
+		Sol1 sol = new Sol1();
+		System.out.println(sol.solution(phone_book) + "");
+	}
+	
+}
