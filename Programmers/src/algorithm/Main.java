3eb02c416c3accc71c9bf9625f7bf42c56eb0eb7
kim-hyoungwoo <dicjqdlwld@gmail.com>
2022-07-15 22:19:13 +0900

    test

--- Programmers/src/algorithm/Main.java
+++ Programmers/src/algorithm/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		/**
-		 * 전화번호 목록
+		 * 프로그래머스(해시) - 전화번호 목록
 		 */
 		String[] phone_book = {"119", "97674223", "1195524421"};
 		Sol1 sol = new Sol1();
