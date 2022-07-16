fa24a7fad34025fa0cdd75e76b39aa1834ac1a63
kim-hyoungwoo <dicjqdlwld@gmail.com>
2022-07-16 23:25:34 +0900

    삽입 정렬까지 공부함

--- AlgorithmStudy/src/algorithm/Main.java
+++ AlgorithmStudy/src/algorithm/Main.java
@@ -3,9 +3,8 @@ package algorithm;
 public class Main {
 
 	public static void main(String[] args) {
-		SelectionSort sort = new SelectionSort();
+		Sort sort = new Sort();
 		sort.selectionSort();
-		// 이건 어떻게 되나요? 테스트 이건요:
 	}
 
 }
