1e65950b312de40f6a7d4b5abadcb853b7a608dd
kim-hyoungwoo <dicjqdlwld@gmail.com>
2022-07-20 00:30:06 +0900

    동빈나 유튜브 힙정렬 공부

--- AlgorithmStudy/src/algorithm/Main.java
+++ AlgorithmStudy/src/algorithm/Main.java
@@ -4,22 +4,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		Sort sort = new Sort();
-		//sort.selectionSort();
+		sort.selectionSort();
 		
-		/*int num = 10;
-		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
-		
-		sort.quickSort(array, 0, num-1);
-		for (int i : array) {
-			System.out.print(i);
-		}*/
-		
-		int[] array = {7, 6, 5, 8, 3, 5, 9, 1};
-		sort.mergeSort(array, 0, array.length-1);
-		for (int i : array) {
+		int num = 10;
+		int[] quickArray = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+		System.out.println("");
+		sort.quickSort(quickArray, 0, num-1);
+		for (int i : quickArray) {
 			System.out.print(i);
 		}
+		System.out.println("");
+		int[] mergeArray = {7, 6, 5, 8, 3, 5, 9, 1};
+		sort.mergeSort(mergeArray, 0, mergeArray.length-1);
+		for (int i : mergeArray) {
+			System.out.print(i);
+		}
+		System.out.println("");
+		int heapNum = 9;
+		int[] heapArray = {7, 6, 5, 8, 3, 5, 9, 1, 6};
 		
+		sort.heapSort(heapArray.length, heapArray);
+		
+		for (int i : heapArray) {
+			System.out.println(i);
+		}
 	}
 
 }
--- AlgorithmStudy/src/algorithm/Sort.java
+++ AlgorithmStudy/src/algorithm/Sort.java
@@ -1,12 +1,11 @@
 package algorithm;
 
 public class Sort {
-	
+	/**
+	 * 선택 정렬 (Selection Sort)<br>
+	 * https://youtu.be/8ZiSzteFRYc
+	 */
 	public void selectionSort() {
-		/**
-		 * 선택 정렬 (Selection Sort)<br>
-		 * https://youtu.be/8ZiSzteFRYc
-		 */
 		int min = 0;
 		int index = 0;
 		int temp = 0;
@@ -33,11 +32,11 @@ public class Sort {
 		}
 	}
 	
+	/**
+	 * 버블 정렬 (Bubble Sort)<br>
+	 * https://youtu.be/EZN0Irp2aPs
+	 */
 	public void bubbleSort() {
-		/**
-		 * 버블 정렬 (Bubble Sort)<br>
-		 * https://youtu.be/EZN0Irp2aPs
-		 */
 		int temp = 0;
 		
 		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
@@ -58,11 +57,12 @@ public class Sort {
 		}
 	}
 	
+	/**
+	 * 삽입 정렬 (Insertion Sort)<br>
+	 * https://youtu.be/16I9Z7bS1iM
+	 */
 	public void insertionSort() {
-		/**
-		 * 삽입 정렬 (Insertion Sort)<br>
-		 * https://youtu.be/16I9Z7bS1iM
-		 */
+		
 		int temp = 0;
 		int j = 0;
 		int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
@@ -81,11 +81,12 @@ public class Sort {
 			System.out.print(i);
 		}
 	}
+	
+	/**
+	 * 퀵 정렬(quick sort)<br>
+	 * https://youtu.be/O-O-90zX-U4
+	 */
 	public void quickSort(int[] data, int start, int end) {
-		/**
-		 * 퀵 정렬(quick sort)<br>
-		 * https://youtu.be/O-O-90zX-U4
-		 */
 		// 원소가 1개인 경우 
 		if (start >= end) {
 			return;
@@ -123,7 +124,6 @@ public class Sort {
 	 * 병합 정렬(Merge sort)<br>
 	 * https://youtu.be/ctkuGoJPmAE
 	 */
-	
 	public void mergeSort(int[] array, int start, int end) {
 		// 정중앙을 기점으로 배열을 나눈다. 
 		// 크기가 1보다 큰 경우
@@ -184,4 +184,53 @@ public class Sort {
 		}
 	}
 	
+	/**
+	 * 힙 정렬 (Heap Sort)<br>
+	 * https://youtu.be/iyl9bfp_8ag
+	 */
+	public void heapSort(int num, int[] heap) {
+		// 먼저 전체 트리 구조를 최대 힙 구조로 바꾼다.
+		for (int i = 0; i < num; i++) {
+			int current = i;
+			do {
+				// root 는 지금 자신의 -1 을 하고 2로 나누면 나온다.
+				int root = (current - 1) / 2;
+				// 부모의 값이 자식의 값보다 크다면
+				if (heap[root] < heap[current]) {
+					int temp = heap[root];
+					heap[root] = heap[current];
+					heap[current] = temp;
+				}
+				current = root;
+			} while (current != 0);
+		}
+		// 크기를 줄여가며 반복적으로 힙을 구성
+		for (int i = num - 1; i >= 0; i--) {
+			// 0번의 인덱스 값과 마지막 값을 바꿔 정렬한다. 
+			int temp = heap[0];
+			heap[0] = heap[i];
+			heap[i] = temp;
+			// 초기화
+			int root = 0;
+			// 현재 위치
+			int current = 1;
+			do {
+				//root 의 자식 계산
+				current = 2 * root + 1;
+				
+				// 자식 중에 더 큰 값을 찾는다. 
+				// 오른쪽과 왼쪽을 비교했을때 어떤게 더 큰지 비교해서 root 랑 비교
+				if (current < i-1 && heap[current] < heap[current + 1]) {
+					current++;
+				}
+				// 루트보다 자식이 더 크다면 교환
+				if (heap[root] < heap[current] && current < i ) {
+					int temp2 = heap[root];
+					heap[root] = heap[current];
+					heap[current] = temp2;
+				}
+				root = current;
+			} while (current < i);
+		}
+	}
 }
